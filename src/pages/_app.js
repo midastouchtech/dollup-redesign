a367fe5c6a5cffda5b1b8255bdf5dc52844c39b2
Ayabonga Qwabi <aya@simply.co.za>
2024-03-12 20:52:14 +0200

    temporarily remove progressbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,10 @@
 import "@/styles/globals.css";
-import NextNProgress from "nextjs-progressbar";
 import { Provider } from "react-redux";
 import store from "@/store";
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Provider store={store}>
-        <NextNProgress color={"pink"} />
         <Component {...pageProps} />;
       </Provider>
     </>
