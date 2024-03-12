dda96dfbce3c6b5d52f038d83166a6de947191f4
Ayabonga Qwabi <aya@simply.co.za>
2024-03-12 20:45:57 +0200

    add dashboard button

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -122,7 +122,7 @@ const Navigation = ({user}) => {
             <div className='flex items-center space-x-6 rtl:space-x-reverse'>
             <Link
               href="/dashboard"
-              className='hidden md:block text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
+              className=' text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
             >
               Dashboard
             </Link>
